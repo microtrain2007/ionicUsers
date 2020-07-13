b7c0f81b7805c7113e4eba19c152583276b76480
chad svastisalee <swasgrafx@gmail.com>
2020-07-13 17:04:58 -0500

    Added ability to view Users and Single User

---
+++ src/app/users/users.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { User } from '../user.model';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.page.html',
+  styleUrls: ['./users.page.scss'],
+})
+export class UsersPage implements OnInit {
+  users:User;
+
+  constructor(private usersService:UsersService) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  public getUsers(): void{
+    this.usersService.getUsers().subscribe(
+      (response:any) => {
+        // console.log = (response);
+        this.users = response.users;
+      }
+    );
+  }
+
+} 
